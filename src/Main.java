182b86ab4c85131c99cd4373428cf4924b71656d
Dmitriy <dmitriyndubov@gmail.com>
2025-02-06 16:47:27 +0500

    homework on conditional statements lesson_2, completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,91 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Задача 1
+        System.out.print("Введите 0 для iOS или 1 для Android: ");
+        int clientOS = scanner.nextInt();
+        System.out.println("Результат задачи 1:");
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        // Задача 2
+        System.out.print("Введите год выпуска устройства: ");
+        int clientDeviceYear = scanner.nextInt();
+        System.out.println("Результат задачи 2:");
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        // Задача 3
+        System.out.print("Введите год для проверки на високосность: ");
+        int year = scanner.nextInt();
+        System.out.println("Результат задачи 3:");
+        if (year > 1584 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        // Задача 4
+        System.out.print("Введите расстояние доставки: ");
+        int deliveryDistance = scanner.nextInt();
+        int deliveryDays = 1;
+        System.out.println("Результат задачи 4:");
+        if (deliveryDistance > 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 100) {
+            System.out.println("Доставка не осуществляется");
+        } else {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        }
+
+        // Задача 5
+        System.out.print("Введите номер месяца: ");
+        int monthNumber = scanner.nextInt();
+        System.out.println("Результат задачи 5:");
+        if (monthNumber < 1 || monthNumber > 12) {
+            System.out.println("Некорректный номер месяца");
+        } else {
+            switch (monthNumber) {
+                case 12:
+                case 1:
+                case 2:
+                    System.out.println("Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Осень");
+                    break;
+            }
+        }
+
+        scanner.close();
     }
 }
